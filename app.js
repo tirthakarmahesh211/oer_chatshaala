cc53510f19767818af1ec3b1c3863ca3e5c43f3d
Bir Anmol <f20180261@pilani.bits-pilani.ac.in>
2020-05-24 15:05:31 +0530

    second commit

--- app.js
+++ app.js
@@ -3,54 +3,105 @@ const express = require('express');
 const app = express();
 const ejs = require("ejs");
 const parser = require('body-parser');
-const data = {
-  email: '',
-  password: '',
-  fname: '',
-  lname: '',
-  notif: 'on',
-  remember: 'off'
-};
+const users=[];
+const md5=require('md5');
+function resetCurrUser(){
+  return new getUser('','','','','on','off',false);
+}
+function getUser(email,password,fname,lname,notif,remember,login){
+  this.email=email;
+  this.password=password;
+  this.fname=fname;
+  this.lname=lname;
+  this.notif=notif;
+  this.remember=remember;
+  this.login=login;
+  this.getName=function(){
+    return this.fname+' '+this.lname;
+  };
+}
+
+function verifyUser(curr_user){
+  for(var i=0;i<users.length;i++){
+    var user=users[i];
+    if(curr_user.email === user.email && curr_user.password === user.password){
+      curr_user.fname=user.fname;
+      curr_user.lname=user.lname;
+      curr_user.notif=user.notif;
+      user.remember=curr_user.remember;
+      curr_user.login=true;
+      user.login=true;
+      return {status: true,index:i};
+    }
+  }
+    return {status: false, index:-1};
+}
+
 
 app.set('view engine', 'ejs');
 app.use(parser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+
+
 app.listen(3000, function(req, res) {
-  console.log('Server Started');
+  console.log('Server Started on localhost:3000');
 });
+
 app.get('/', function(req, res) {
   res.render('register.ejs',{status:''});
 });
 
 app.post('/', function(req, res) {
   if (req.body.hasOwnProperty('signup')) {
-    data.email = req.body.email1;
-    data.password = req.body.pass1;
-    data.fname = req.body.fname;
-    data.lname = req.body.lname;
+
     if (req.body.hasOwnProperty('notifications')) {
-      data.notif = req.body.notifications;
+      users.push(new getUser(req.body.email1,md5(req.body.pass1),req.body.fname,req.body.lname,req.body.notifications,'off',false));
+    }else{
+      users.push(new getUser(req.body.email1,md5(req.body.pass1),req.body.fname,req.body.lname,'on','off',false));
     }
     //send everything to db
+    
     res.render('register.ejs',{status:'Successfuly Registered, Kindly Login'});
-  } else if(req.body.hasOwnProperty('login')){
-      data.email = req.body.email2;
-      data.password = req.body.pass2;
-      //data.fname = req.body.fname;//Populte from data base
-      //data.lname = req.body.lname;//populate from database
-    if (req.body.hasOwnProperty('remember')) {
-      data.remember = req.body.remember;
-    }else{
-      //fetch from database
-    }
-    //fetch notif from db
-    res.render('home.ejs');
-  }else if(req.body.hasOwnProperty('forgot')){
-    res.send('Forgot Password');
+  }
+   else if(req.body.hasOwnProperty('login')){
+      var curr_user=resetCurrUser();
+      curr_user.email = req.body.email2;
+      curr_user.password = md5(req.body.pass2);
+      if (req.body.hasOwnProperty('remember')) {
+        curr_user.remember = req.body.remember;
+      }else{
+        curr_user.remember='off';
+      }
+      //Check in database
+      const info=verifyUser(curr_user);
+      if(info.status === true){
+        res.redirect('/home?user='+info.index);
+      }else{
+        res.render('register.ejs',{status:'Not Registered yet, please register before login'});
+      }
+  }
+  else if(req.body.hasOwnProperty('forgot')){
+    res.send('forgot password page');
   }
   else{
     res.send('Error Occured');
+  }
+});
+
+app.get('/home',function(req,res){
+  if(Object.keys(req.query).length!==0){
+  const index=req.query.user;
+  if(index<users.length && index>=0 && users[index].login === true){
+    users[index].login=false;
+    var curr_user=users[index];
+    res.render('home.ejs',{name:curr_user.getName()});
+  }else{
+    res.redirect('/');
+  }
+}else{
+    //invalid login attempt
+    res.redirect('/');
   }
 });
--- public/js/register.js
+++ public/js/register.js
@@ -1,12 +1,13 @@
-$(document).ready(function()
-{
-$("#choice1").click(function()
-{
-$("#Login").removeClass('hidden');
-$("#SignUp").addClass('hidden');
+
+var choice1=document.querySelector('#choice1');
+var choice2=document.querySelector('#choice2');
+var login=document.querySelector('#Login');
+var sign=document.querySelector('#SignUp');
+choice1.addEventListener('click',function(){
+  login.classList.remove('hidden');
+  sign.classList.add('hidden');
 });
-$('#choice2').click(function(){
-  $("#Login").addClass('hidden');
-  $("#SignUp").removeClass('hidden');
-});
+choice2.addEventListener('click',function(){
+  login.classList.add('hidden');
+  sign.classList.remove('hidden');
 });
