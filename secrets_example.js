88defc45ec189d87c0d99abca384351e192713e7
Bir Anmol <f20180261@pilani.bits-pilani.ac.in>
2020-06-07 19:22:03 +0530

    added secrets_example.js

---
+++ secrets_example.js
@@ -0,0 +1,9 @@
+//jshint esversion: 8
+const API_KEY='';//Your API key here
+const URL='';//The url to the endpoint of API goes here
+const long_string='';//Any random long string
+module.exports.key=API_KEY;
+module.exports.url=URL;
+module.exports.string=long_string;
+
+//Rename this file to secrets.js and add your values for the variables.
